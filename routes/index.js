4f1a7ac3a992997dabf7684c832f45788d6eb41f
沈林强 <942934068@qq.com>
2021-05-26 10:32:35 +0800

    初次上传

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const jwt = require('jsonwebtoken');
+const secret = 'Spider-Man'
+
 const { UserModel, ChatModel } = require('../db/models');
 const md5 = require('blueimp-md5')
 const filter = { password: 0, __v: 0}
@@ -35,6 +38,7 @@ router.post('/login', function(req, res){
   const { username, password } = req.body
   UserModel.findOne({username, password :md5(password)}, filter, function(error, user){
     if(user) {
+
       res.cookie('userid', user._id, {maxAge: 1000*60*60*24})
       res.send({code: 0, data: user})
     } else {
@@ -91,17 +95,42 @@ router.get('/userList', function (req, res) {
 })
 
 router.get('/msglist', function (req, res) {
+  console.log('msglist')
   const userid = req.cookies.userid
   //找出所有user 组成所需的数据结构
   UserModel.find(function (error, userDocs) {
+    // const users = {} // 对象容器
+    // userDocs.forEach(doc => {
+    //   users[doc._id] = {username: doc.username, header: doc.header}
+    // })
     const users = userDocs.reduce((users, user)=>{
       users[user._id] = {username: user.username, header: user.header}
       return users
     },{})
+    ChatModel.find({'$or':[{from: userid}, {to: userid}]}, filter, function (err, chatMsgs) {
+      // console.log('ChatModel',chatMsgs)cl
+      res.send({code: 0, data: {users, chatMsgs}})
+    })
   })
-  ChatModel.find({'$or':[{from: userid}, {to: userid}], filter, function (error, chatMsgs) {
-    res.send({code: 0, data: {users, chatMsgs}})
-  }})
+})
+
+router.post('/readmsg',function(req, res){
+  const from = req.body.from
+  const to = req.cookies.userid
+  // console.log(from)
+  // console.log(to)
+    /*
+  更新数据库中的chat数据
+  参数1: 查询条件
+  参数2: 更新为指定的数据对象
+  参数3: 是否1次更新多条, 默认只更新一条
+  参数4: 更新完成的回调函数
+   */
+  //找到谁发给我的那条记录 from谁 to我
+  ChatModel.updateMany({from, to, read: false}, {'$set':{read: true}}, function(err, doc){
+    // console.log('/readmsg', doc)
+    res.send({code: 0, data: doc.nModified})
+  })
 })
 
 module.exports = router;
